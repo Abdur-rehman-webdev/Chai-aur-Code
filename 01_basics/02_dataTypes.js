eb7d3bc9e227872559de0abb6002706d583a85cf
Abdur-rehman <125436303+Abdur-rehman-webdev@users.noreply.github.com>
2025-01-11 07:52:23 +0000

    ConvertionOperation

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict" // this keyword will use javascript new techniques and versions
+
+//alert('Hello') // We are using node js, not browser
+
+// Code readability should be high
+// .........................................................
+
+// let age = 12;
+// let password = "Maani";
+// let isLoggedIn = true;
+// let hopes;
+// let temparature = null;
+
+// console.log(typeof age);
+ 
+// .........................................................
+
+// const userInput = prompt("Enter your name: ")
+
+// function strLengthCheck() {
+    
+//     if (userInput.length >= 12) {
+//         return userInput + "..."
+//     } else {
+//         return userInput
+//     }
+// }
+
+
+// console.log(strLengthCheck());
+
+// ...............................................................
