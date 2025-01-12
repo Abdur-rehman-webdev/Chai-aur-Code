44cbade6cc340084d481f7a7fe6741696dcdb200
Abdur-rehman <125436303+Abdur-rehman-webdev@users.noreply.github.com>
2025-01-12 13:32:06 +0000

    Prctice

---
+++ 01_basics/exersise'sPractice.js
@@ -0,0 +1,40 @@
+
+// Exercise Program to reverse the string
+
+// function reverseString (str){
+//       return str.split("").reverse().join("");
+// }
+
+// console.log(reverseString("Perfume"))
+
+// .............................................
+
+// Exercise Program to find Largest Number in a Array
+
+// function findLargestNumber(arr){
+//         return Math.max(...arr);
+// }
+// console.log(findLargestNumber([33,44,22,55,33,66,77])) 
+
+// ..............................................
+
+// // fizzBuzz Program Exercise
+
+// function fizzBuzz(){
+//   for(i=1; i <= 100; i++){
+//       if(i % 3 === 0 && i % 5 === 0 ){
+//         console.log("fizzBuzz");
+//       }
+//       else if (i % 3 === 0){
+//         console.log("fizz");
+//       }
+//       else if (i % 5 === 0){
+//         console.log("buzz");
+//       }
+//       else{
+//         console.log(i);
+//       }
+//   }
+// }
+
+// fizzBuzz();
